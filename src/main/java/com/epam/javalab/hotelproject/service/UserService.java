ce2cace6556168c8f0835f17935ae65bc9967385
Maksim Starshinov <jebsuz@gmail.com>
2017-07-31 17:51:28 +0300

    registerUser completed(?)

--- src/main/java/com/epam/javalab/hotelproject/service/UserService.java
+++ src/main/java/com/epam/javalab/hotelproject/service/UserService.java
@@ -4,17 +4,27 @@ import com.epam.javalab.hotelproject.model.User;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class UserService {
+    private SecurityService securityService = new SecurityService();
 
     public boolean registerUser(User user) {
-        // TODO
-        if (loginIsAvailable("")) {
-
+        if (!validateUserBean(user)) {
+            return false;
         }
-        throw new NotImplementedException();
+
+        if (!loginIsAvailable(user.getLogin())) {
+            return false;
+        }
+
+        user.setPassword(securityService.hash(user.getPassword()));
+
+        return saveUser(user);
     }
 
     public boolean authorize(User user) {
-        SecurityService securityService = new SecurityService();
+        throw new NotImplementedException();
+    }
+
+    private boolean saveUser(User user) {
         throw new NotImplementedException();
     }
 
@@ -22,6 +32,18 @@ public class UserService {
         // TODO
         getUserByLogin(login);
         throw new NotImplementedException();
+    }
+
+    private boolean validateUserBean(User user) {
+        if (user.getLogin() == null || user.getPassword() == null || user.getEmail() == null) {
+            return false;
+        }
+
+        if (user.getLogin().isEmpty() || user.getPassword().isEmpty() || user.getEmail().isEmpty()) {
+            return false;
+        }
+
+        return true;
     }
 
     private User getUserByLogin(String login) {
