5293cee6f3046e441b8228d245cdbf09bedbc000
sergeipetriankin <gekreuzigt2012@gmail.com>
2017-07-31 18:25:33 +0300

    SecurityService class is released.

--- src/main/java/com/epam/javalab/hotelproject/Main.java
+++ src/main/java/com/epam/javalab/hotelproject/Main.java
@@ -19,5 +19,6 @@ public class Main {
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
         tomcat.getServer().await();
+
     }
 }
--- src/main/java/com/epam/javalab/hotelproject/service/SecurityService.java
+++ src/main/java/com/epam/javalab/hotelproject/service/SecurityService.java
@@ -2,8 +2,21 @@ package com.epam.javalab.hotelproject.service;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.security.MessageDigest;
+import java.util.Arrays;
+
 public class SecurityService {
     public String hash(String password) {
-        throw new NotImplementedException();
+        String result = password;
+        try {
+            byte[] bytesOfMessage = password.getBytes("UTF-8");
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] theDigest = md.digest(bytesOfMessage);
+            result = Arrays.toString(theDigest);
+        }
+        catch (Exception e) {
+            //TODO log4j
+        }
+        return result;
     }
 }
