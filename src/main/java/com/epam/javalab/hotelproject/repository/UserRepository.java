8e74f07d93adb8527d0d635c767ef9118552763f
Maksim Starshinov <jebsuz@gmail.com>
2017-08-01 19:09:18 +0300

    added UserDAO findAll implementation with simple test

--- src/main/java/com/epam/javalab/hotelproject/repository/UserRepository.java
+++ src/main/java/com/epam/javalab/hotelproject/repository/UserRepository.java
@@ -1,20 +1,39 @@
 package com.epam.javalab.hotelproject.repository;
 
 import com.epam.javalab.hotelproject.model.User;
+import com.epam.javalab.hotelproject.service.DataBaseService;
+import com.epam.javalab.hotelproject.service.DataBaseServiceImpl;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 // TODO
 public class UserRepository implements UserDAO {
+    DataBaseService dataBaseService = DataBaseServiceImpl.getInstance();
+
     @Override
     public List<User> findAll() {
-        return null;
+        List users = new ArrayList();
+        try (Connection connection = dataBaseService.takeConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT * FROM sql11188080.users");) {
+            while (resultSet.next()) {
+                users.add(new User(resultSet.getString("first_name"), resultSet.getString("password"), resultSet.getString("email")));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     @Override
     public User findByLogin(String login) {
-        return new User("", "", "", "");
+        return new User("", "", "");
     }
 
     @Override
