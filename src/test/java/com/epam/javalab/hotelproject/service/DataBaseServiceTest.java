ddffeed67ed9a008dffd3f0e17a8a44e5754b4b2
sergeipetriankin <gekreuzigt2012@gmail.com>
2017-08-01 14:11:00 +0300

    DataBaseService implementation version 1.0. Tested.

---
+++ src/test/java/com/epam/javalab/hotelproject/service/DataBaseServiceTest.java
@@ -0,0 +1,23 @@
+package com.epam.javalab.hotelproject.service;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import static org.junit.Assert.*;
+
+public class DataBaseServiceTest {
+    DataBaseService dataBaseService = DataBaseService.getInstance();
+
+    @Test
+    public void test() throws Exception {
+        DataBaseService.PooledConnection connection = dataBaseService.takeConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM sql11188080.test");
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+        }
+    }
+}
