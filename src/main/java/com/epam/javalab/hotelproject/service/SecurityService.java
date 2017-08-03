2ddf943281b26cf4d6b960f84a08f67aacb6cc1b
Maksim Starshinov <jebsuz@gmail.com>
2017-08-03 19:04:59 +0300

    Merge branch 'Team2'
    # Conflicts:
    #	src/main/java/com/epam/javalab/hotelproject/service/SecurityService.java
    #	src/test/java/com/epam/javalab/hotelproject/repository/UserRepositoryTest.java

--- src/main/java/com/epam/javalab/hotelproject/service/SecurityService.java
+++ src/main/java/com/epam/javalab/hotelproject/service/SecurityService.java
@@ -9,47 +9,9 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
 
-/**
- * Provides API fo hashing password and suthorization
- *
- * @author Maksim Starshinov, Sergei Petriankin
- * @version 1.0
- *
- */
 
-public class SecurityService {
-    private UserDAO userDAO = new UserRepository();
+public interface SecurityService {
+    public String hash(String password);
 
-    /**
-     *
-     * @param password
-     * @return hashed password using MD5 algorithm
-     */
-    public String hash(String password) {
-        String result = password;
-        try {
-            byte[] bytesOfMessage = password.getBytes("UTF-8");
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] theDigest = md.digest(bytesOfMessage);
-            result = new String(theDigest, StandardCharsets.UTF_8);
-        } catch (Exception e) {
-            //TODO log4j
-        }
-        return result;
-    }
-
-    /**
-     *
-     * @param user
-     * @return <code>true</code> if such user is is already registered
-     */
-    public boolean authorize(User user) {
-        User registeredUser = userDAO.findByLogin(user.getLogin());
-        if (registeredUser.getLogin().isEmpty()) {
-            return false;
-        }
-        return hash(user.getPassword()).equals(registeredUser.getPassword());
-
-
-    }
+    public boolean authorize(User user);
 }
--- src/main/java/com/epam/javalab/hotelproject/service/SecurityServiceImpl.java
+++ src/main/java/com/epam/javalab/hotelproject/service/SecurityServiceImpl.java
@@ -9,9 +9,20 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
 
+/**
+ * Provides API fo hashing password and suthorization
+ *
+ * @author Maksim Starshinov, Sergei Petriankin
+ * @version 1.0
+ */
+
 public class SecurityServiceImpl implements SecurityService {
     private UserDAO userDAO = new UserRepository();
 
+    /**
+     * @param password
+     * @return hashed password using MD5 algorithm
+     */
     public String hash(String password) {
         String result = password;
         try {
@@ -25,12 +36,17 @@ public class SecurityServiceImpl implements SecurityService {
         return result;
     }
 
+    /**
+     * @param user
+     * @return <code>true</code> if such user is is already registered
+     */
     public boolean authorize(User user) {
         User registeredUser = userDAO.findByLogin(user.getLogin());
         if (registeredUser.getLogin().isEmpty()) {
             return false;
         }
-
         return hash(user.getPassword()).equals(registeredUser.getPassword());
+
+
     }
 }
