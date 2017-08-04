2996417c28a2d709c88ec96a50c0c0e01b4a96d0
sergeipetriankin <gekreuzigt2012@gmail.com>
2017-08-04 13:58:43 +0300

    RoomRepository findByNumber was added.

--- src/main/java/com/epam/javalab/hotelproject/repository/RoomRepository.java
+++ src/main/java/com/epam/javalab/hotelproject/repository/RoomRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class RoomRepository implements RoomDAO {
     DatabaseService databaseService = DatabaseServiceImpl.getInstance();
     private Room emptyRoom = new Room();
+
     @Override
     public List<Room> finAll() {
         List<Room> roomList = new ArrayList<>();
@@ -18,10 +19,10 @@ public class RoomRepository implements RoomDAO {
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT * FROM sql11188080.rooms ")) {
             while (resultSet.next()) {
-                 roomList.add(new Room(resultSet.getInt("id"),
-                                        resultSet.getInt("number"),
-                                        resultSet.getInt("beds"),
-                                        resultSet.getInt("id_class")));
+                roomList.add(new Room(resultSet.getInt("id"),
+                        resultSet.getInt("number"),
+                        resultSet.getInt("beds"),
+                        resultSet.getInt("id_class")));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -31,7 +32,32 @@ public class RoomRepository implements RoomDAO {
 
     @Override
     public Room findByNumber(int number) {
-      return null;
+        if (number == 0) {
+            return emptyRoom;
+        }
+        ResultSet resultSet = null;
+        try (Connection connection = databaseService.takeConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM sql11188080.rooms WHERE number = ?")) {
+            preparedStatement.setInt(1, number);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return new Room(resultSet.getInt("id"),
+                        resultSet.getInt("number"),
+                        resultSet.getInt("beds"),
+                        resultSet.getInt("id_class"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null && !resultSet.isClosed()) {
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return emptyRoom;
+        }
     }
 
     @Override
@@ -59,3 +85,4 @@ public class RoomRepository implements RoomDAO {
         return false;
     }
 }
+
