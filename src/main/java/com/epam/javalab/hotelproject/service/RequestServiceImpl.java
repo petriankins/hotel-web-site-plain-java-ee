d4fc13615af70d29fb1c8d20a4fa329e9dde17cf
myaichnikov <ldamaxxl@gmail.com>
2017-08-04 18:56:43 +0300

    RequestDAO and RequestRepositroy updated: findByUser method added

--- src/main/java/com/epam/javalab/hotelproject/service/RequestServiceImpl.java
+++ src/main/java/com/epam/javalab/hotelproject/service/RequestServiceImpl.java
@@ -32,7 +32,7 @@ public class RequestServiceImpl implements RequestService {
 
     @Override
     public List<Request> findByUser(User user) {
-        return req
+        return requestDAO.findByUser(user);
     }
 
     private boolean createRequest(Request request) {
