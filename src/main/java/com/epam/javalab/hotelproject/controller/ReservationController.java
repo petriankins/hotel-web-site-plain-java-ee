77d85d4ba023388698e29a3dad7afa327d0bdd49
Maksim Starshinov <jebsuz@gmail.com>
2017-08-07 12:46:56 +0300

    requests are now being processed by server

--- src/main/java/com/epam/javalab/hotelproject/controller/ReservationController.java
+++ src/main/java/com/epam/javalab/hotelproject/controller/ReservationController.java
@@ -1,5 +1,10 @@
 package com.epam.javalab.hotelproject.controller;
 
+import com.epam.javalab.hotelproject.model.Request;
+import com.epam.javalab.hotelproject.model.User;
+import com.epam.javalab.hotelproject.service.RequestService;
+import com.epam.javalab.hotelproject.service.RequestServiceImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +12,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 @WebServlet(
         name = "ReservationServlet",
         urlPatterns = {"/order"}
 )
 public class ReservationController extends HttpServlet {
+    RequestService requestService = new RequestServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -28,23 +38,44 @@ public class ReservationController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        User user = (User) session.getAttribute("user");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        int beds = 0;
+        int stars = 0;
+        Date defaultDate = new Date();
+        Date dateFrom = defaultDate;
+        Date dateTo = defaultDate;
 
-        String beds = req.getParameter("beds");
-        String stars = req.getParameter("stars");
-        String checkIn = req.getParameter("checkIn");
-        String checkOut = req.getParameter("checkOut");
+        try {
+            beds = Integer.parseInt(req.getParameter("beds").trim());
+            stars = Integer.parseInt(req.getParameter("stars").trim());
+            dateFrom = dateFormat.parse(req.getParameter("checkIn").trim());
+            dateTo = dateFormat.parse(req.getParameter("checkOut").trim());
+            System.out.println("From: " + dateFrom);
+            System.out.println("To: " + dateTo);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Request request = new Request((new Random().nextInt()), user.getId(), beds, stars, dateFrom, dateTo, "");
 
         String message = null;
 
-        if (beds.equals("") || stars.equals("") || checkIn.equals("") || checkOut.equals("")) {
+        if (request.getBeds() == 0 || request.getClassID() == 0 || request.getDateFrom().equals(defaultDate) ||
+            request.getDateTo().equals(defaultDate)) {
             message = "Fill all fields please";
             req.setAttribute("message", message);
             req.getRequestDispatcher("/jsp/order.jsp").forward(req, resp);
         } else {
-            message = "Reservation have gone correct. Bill will be email to you";
+            if (requestService.saveRequest(request)) {
+                message = "Reservation have gone correct. Bill will be email to you";
+            } else {
+                message = "Something went wrong : (";
+            }
             req.setAttribute("message", message);
             req.getRequestDispatcher("/jsp/order.jsp").forward(req, resp);
-            System.out.println(beds + stars + checkIn + checkOut);
+
         }
     }
 }
