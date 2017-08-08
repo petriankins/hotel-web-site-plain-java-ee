c590c85a1137146db9f1d19116112ecc37b9c265
Andrei <volk-nora@list.ru>
2017-08-08 19:08:37 +0300

    billController hotfix

--- src/main/java/com/epam/javalab/hotelproject/controller/BillController.java
+++ src/main/java/com/epam/javalab/hotelproject/controller/BillController.java
@@ -13,7 +13,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static javax.swing.text.html.CSS.getAttribute;
 
 @WebServlet(
         name = "billServlet",
