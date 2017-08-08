23e775a4983140c9dc9990c8f604ee6026c10741
Andrei <volk-nora@list.ru>
2017-08-08 17:21:48 +0300

    bill creation in BillController added

--- src/main/java/com/epam/javalab/hotelproject/controller/BillController.java
+++ src/main/java/com/epam/javalab/hotelproject/controller/BillController.java
@@ -1,6 +1,10 @@
 package com.epam.javalab.hotelproject.controller;
 
+import com.epam.javalab.hotelproject.model.Bill;
+import com.epam.javalab.hotelproject.model.Request;
 import com.epam.javalab.hotelproject.model.Room;
+import com.epam.javalab.hotelproject.service.BillService;
+import com.epam.javalab.hotelproject.service.BillServiceImpl;
 import com.epam.javalab.hotelproject.service.RoomService;
 import com.epam.javalab.hotelproject.service.RoomServiceImpl;
 
@@ -9,7 +13,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+
+import static javax.swing.text.html.CSS.getAttribute;
 
 @WebServlet(
         name = "billServlet",
@@ -17,12 +24,21 @@ import java.io.IOException;
 )
 public class BillController extends HttpServlet {
     RoomService roomService = new RoomServiceImpl();
+    private BillService billService = new BillServiceImpl();
 
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String roomNumber = req.getParameter("roomNumber");
+
+        HttpSession session = req.getSession();
+        Request request = (Request) session.getAttribute("request");
+
+        System.out.println("request # " + request.getNumber());
         System.out.println(roomNumber);
+
+        Bill bill = billService.createBill(request);
+        System.out.println("Total sum " + bill.getSum());
         req.getRequestDispatcher("/jsp/bill.jsp").forward(req, resp);
     }
 }
