15966967a3108004e2bbfa8daa8f4580e7bda1a4
sergeipetriankin <gekreuzigt2012@gmail.com>
2017-08-08 13:20:35 +0300

    BillDAO's added

---
+++ src/main/java/com/epam/javalab/hotelproject/repository/BillDAO.java
@@ -0,0 +1,17 @@
+package com.epam.javalab.hotelproject.repository;
+
+import com.epam.javalab.hotelproject.model.Bill;
+
+import java.util.List;
+
+public interface BillDAO {
+    List<Bill> findAll();
+
+    Bill findByNumber(int number);
+
+    boolean insertBill(Bill bill);
+
+    boolean updateBill(Bill bill);
+
+    boolean deleteBill(Bill bill);
+}
