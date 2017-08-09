166433ce9b76fbc770bcc90329f34cb2abaff881
Maksim Starshinov <jebsuz@gmail.com>
2017-08-09 15:37:57 +0300

    wut

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -2,3 +2,21 @@ function switchLang(node) {
     var currentPath = encodeURIComponent(window.location.href.replace(window.location.protocol + "//" + window.location.host, ""));
     window.location.href="/locale?lang=" + node.value + "&redirectTo=" + currentPath;
 }
+
+function checkForm(form) {
+
+    if (form.password.value != "" && form.password.value == form.passwordConfirm.value) {
+        if (form.password.value.length < 6) {
+            alert("Error: Password must contain at least six characters!");
+            form.password.focus();
+            return false;
+        }
+    } else {
+        alert("Error: Please check that you've entered and confirmed your password!");
+        form.password.focus();
+        return false;
+    }
+
+    alert("You entered a valid password!");
+    document.getElementById('submitBtn').click();
+}
