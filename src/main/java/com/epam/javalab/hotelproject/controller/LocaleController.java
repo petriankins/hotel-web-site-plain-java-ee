141e1252e724f1d3565885e61769a157ba750cd4
Maksim Starshinov <jebsuz@gmail.com>
2017-08-09 14:49:16 +0300

    Added redirect back to where we was when switching lang

--- src/main/java/com/epam/javalab/hotelproject/controller/LocaleController.java
+++ src/main/java/com/epam/javalab/hotelproject/controller/LocaleController.java
@@ -1,5 +1,8 @@
 package com.epam.javalab.hotelproject.controller;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.NOPLogger;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,20 +10,26 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @WebServlet(
         name = "LocaleController",
         urlPatterns = "/locale"
 )
 public class LocaleController extends HttpServlet {
+    private final static Logger LOGGER = Logger.getLogger(LocaleController.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
         String lang = req.getParameter("lang").trim();
+        String url = req.getParameter("redirectTo").trim();
+        LOGGER.debug("Got url: " + url);
         if (lang != null) {
             session.setAttribute("lang", lang);
         }
 
-        resp.sendRedirect("/");
+        resp.sendRedirect(url);
     }
 }
--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,3 +1,4 @@
 function switchLang(node) {
-    window.location.href="/locale?lang=" + node.value;
+    var currentPath = encodeURIComponent(window.location.href.replace(window.location.protocol + "//" + window.location.host, ""));
+    window.location.href="/locale?lang=" + node.value + "&redirectTo=" + currentPath;
 }
