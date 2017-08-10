347fe09e3991d9ab1b0a51b63ad797a567928ffe
Maksim Starshinov <jebsuz@gmail.com>
2017-08-10 12:47:31 +0300

    New method to stop app with closing DB connection pool

--- src/main/java/com/epam/javalab/hotelproject/Main.java
+++ src/main/java/com/epam/javalab/hotelproject/Main.java
@@ -1,23 +1,44 @@
 package com.epam.javalab.hotelproject;
 
 import java.util.Optional;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import com.epam.javalab.hotelproject.service.DatabaseServiceImpl;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private final static Logger LOGGER = Logger.getLogger(Main.class);
 
-    public static final Optional<String> PORT = Optional.ofNullable(System.getenv("PORT"));
+    public static final Optional<String> PORT     = Optional.ofNullable(System.getenv("PORT"));
     public static final Optional<String> HOSTNAME = Optional.ofNullable(System.getenv("HOSTNAME"));
 
     public static void main(String[] args) throws Exception {
-        String contextPath = "/" ;
+        String contextPath = "/";
         String appBase = ".";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(Integer.valueOf(PORT.orElse("8080") ));
+        tomcat.setPort(Integer.valueOf(PORT.orElse("8080")));
         tomcat.setHostname(HOSTNAME.orElse("localhost"));
         tomcat.getHost().setAppBase(appBase);
         tomcat.addWebapp(contextPath, appBase);
         tomcat.start();
-        tomcat.getServer().await();
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(() -> tomcat.getServer().await());
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            if (scanner.nextLine().equals("stop")) {
+                LOGGER.info("Received STOP command.");
+                DatabaseServiceImpl.getInstance().dispose();
+                LOGGER.info("DatabaseService disposed");
+                break;
+            }
+        }
+        LOGGER.info("Stopping Tomcat");
+        tomcat.stop();
+        LOGGER.info("Good Bye!");
+        System.exit(0);
     }
 }
